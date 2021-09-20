16a2c5a8e4c0e0fe91fb8f96d6066716e5a081ca
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-09-20 20:17:40 +0200

    feat: render mermaid

--- src/renderer/MermaidRenderer.vue
+++ src/renderer/MermaidRenderer.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
-    {{ configJSON }}
+    <div ref="mmdtarget"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Mermaid } from "@/types";
 import { Component, Vue, Prop } from "vue-property-decorator";
+import mermaid from "mermaid";
 
 @Component
 export default class MermaidRendered extends Vue {
   @Prop({ required: true })
   config!: Mermaid;
 
-  get configJSON(): string {
-    return JSON.stringify(this.config);
+  mounted(): void {
+    this._initializeMermaid();
+    this._renderGraph();
+  }
+
+  _initializeMermaid(): void {
+    mermaid.mermaidAPI.initialize({
+      startOnLoad: false,
+    });
+  }
+
+  _renderGraph(): void {
+    const insertSvg = (svgCode: any, _bindFunctions: any) => {
+      (this.$refs.mmdtarget! as any).innerHTML = svgCode;
+    };
+    const id = `mmdtarget-${Math.random()
+      .toString()
+      .substring(2)}`;
+
+    const graphDefinition = this.config.mermaid.mermaid;
+    mermaid.mermaidAPI.render(id, graphDefinition, insertSvg);
   }
 }
 </script>
